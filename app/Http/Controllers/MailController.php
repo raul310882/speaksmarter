14bba10819cab8a92f93ff7854c55d4bade9c083
raul310882 <raul310882@gmail.com>
2024-05-17 16:10:04 -0600

    show pdf in frontend

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use SplSubject;
 
 class MailController extends Controller
 {
@@ -21,14 +23,23 @@ class MailController extends Controller
        
         $inbox = imap_open($mailbox, $user, $password) or die("error en conexion".imap_last_error());
 
-        $emails = imap_search($inbox, 'SUBJECT "Operador" SINCE "1 May 2024"');
+        $emails = imap_search($inbox, 'SUBJECT "Operador" UNSEEN');
 
         if ($emails){
             foreach ($emails as $email_number){
                 $structure = imap_fetchstructure($inbox, $email_number);
 
+                $correos[$email_number] = array (
+                    'subject' => '',
+                    'from' => '',
+                    'main' => '',
+                    'attach' => []
+                );
+               
+
                 $attachments = array();
                 $correo = array();
+                $adjuntos = array();
                 if(isset($structure->parts) && count($structure->parts)) {
 
 	                for($i = 0; $i < count($structure->parts); $i++) {
@@ -37,7 +48,8 @@ class MailController extends Controller
                         'is_attachment' => false,
                         'filename' => '',
                         'name' => '',
-                        'attachment' => ''
+                        'attachment' => '',
+                        'encode' => ''
 		                );
 		
 		                if($structure->parts[$i]->ifdparameters) {
@@ -61,37 +73,78 @@ class MailController extends Controller
                         if($attachments[$i]['is_attachment']) {
                             $attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
                             if($structure->parts[$i]->encoding == 3) { // 3 = BASE64
-                                $attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
+                                /* $data = substr($attachments[$i]['attachment'], strpos($attachments[$i]['attachment'], ',') + 1);
+                                $attachments[$i]['attachment'] = $data; */
+                                $attachments[$i]['encode'] = 'BASE64';
                             }
                             elseif($structure->parts[$i]->encoding == 4) { // 4 = QUOTED-PRINTABLE
                                 $attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
+                                $attachments[$i]['encode'] = 'QUOTED-PRINTABLE';
                             }
-                        }
+                        } 
 	                }
                 }
-
-                
-
-                for ($i = 0; $i < count($attachments); $i++) {
-                    if (!$attachments[$i]['is_attachment']) {
+                $i = 0;
+                foreach ($attachments as $elemento){
+                    if (!$elemento['is_attachment']) {
                         unset ($attachments[$i]);
-                    }
+                    } else {
+                        //$data = substr($attachments[$i]['attachment'], strpos($attachments[$i]['attachment'], ',') + 1);
+                        $pdf_decode = base64_decode($elemento['attachment'], true);
+                        $name = explode(".", $elemento['name']);
+                        $pdf_name = 'C:\xampp\htdocs\SPEAKSMARTER\speaksmarter\storage\app\public\pdf_temp\temp'.$email_number.'cons_'.$i.'_'.$name[0].time().'.pdf';
+                        file_put_contents($pdf_name, $pdf_decode);
+                        $adjuntos[] = 'temp'.$email_number.'cons_'.$i.'_'.$name[0].time().'.pdf';
+                        //$pdf = fopen ($pdf_name, 'w');
+                        //fwrite($pdf, $pdf_decode);
+                        //fclose($pdf);
+                        //$pdf->storeAs('public/pdf_temp', $pdf_name);
+                        //Storage::disk('local')->put($pdf_name, $pdf);
+                    } 
+                    $i++;
                 }
 
-                $correo[] = imap_fetch_overview($inbox, $email_number);
-                $correo[] = str_replace($con_acentos, $sin_acentos, trim(imap_fetchbody($inbox, $email_number, 1.1),"\r\n"));
-                $correo[] = $attachments;
-                $correos[] = $correo;
+
+                /* for ($i = 0; $i < count($attachments); $i++) {
+                    if (!$attachments[$i]['is_attachment']) {
+                        unset ($attachments[$i]);
+                    } else {
+                        //$data = substr($attachments[$i]['attachment'], strpos($attachments[$i]['attachment'], ',') + 1);
+                        $pdf_decode = base64_decode($attachments[$i]['attachment'], true);
+                        $name = explode(".", $attachments[$i]['name']);
+                        $pdf_name = 'C:\xampp\htdocs\SPEAKSMARTER\speaksmarter\storage\app\public\pdf_temp\temp'.$email_number.'cons_'.$i.'_'.$name[0].time().'.pdf';
+                        file_put_contents($pdf_name, $pdf_decode);
+                        $adjuntos[] = 'temp'.$email_number.'cons_'.$i.'_'.$name[0].time().'.pdf';
+                        //$pdf = fopen ($pdf_name, 'w');
+                        //fwrite($pdf, $pdf_decode);
+                        //fclose($pdf);
+                        //$pdf->storeAs('public/pdf_temp', $pdf_name);
+                        //Storage::disk('local')->put($pdf_name, $pdf);
+                    } 
+                } */
+
+
+                $mail = imap_fetch_overview($inbox, $email_number);
+                //dd ($mail[0]->from);
+                $from = mb_convert_encoding($mail[0]->from, 'UTF-8');
+                $subject = $mail[0]->subject;
+                $correos[$email_number]['from'] = $from;
+                $correos[$email_number]['subject'] = $subject;
+                $correos[$email_number]['attach'] = $adjuntos;
+                $main = trim(imap_fetchbody($inbox, $email_number, 1.1),"\r\n");
+                //$main = imap_fetchbody($inbox, $email_number, 1);
+               //$mensaje = imap_qprint($main);
+                //mb_convert_encoding($main, 'UTF-8', 'UTF-8');
+                //$correo[] = $mensaje; 
+                $correos[$email_number]['main'] = $main;
+                //$correo[] = $attachments;
+                //$correos[] = $correo;
+                //mb_convert_encoding($correos['subject'], 'UTF-8', 'UTF-8');
 
             }
 
-
-        dd($correos);
-        
-        
-
-
-        return inertia('Mails/Index');
+        //dd($correos);
+        return inertia('Mails/Index', ['correos' => $correos]);
     }
 }
 
--- resources/js/Pages/Mails/Index.vue
+++ resources/js/Pages/Mails/Index.vue
@@ -9,24 +9,87 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import { Link } from "@inertiajs/vue3";
 import { Inertia } from "@inertiajs/inertia";
 
+defineProps({
+    correos: {
+        type: Object,
+        required: true
+    }
+});
 
+const fileSelected = ref()
+
+const showFile = (_file) => {
+    fileSelected = _file
+}
 
 </script>
 
 <template>
-    <AppLayout title="Lessons">
+    <AppLayout title="Postulantes">
         <template #header>
             <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-                MAILS
+                Mails Received
             </h1>
         </template>
         <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="p-6 bg-white border-b border-gray-200">
-                   
-
-                
                     
+                    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    <li v-for="(correo, index) in correos" :key="index" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                        <div class="flex w-full items-center justify-between space-x-6 p-6">
+                        <div class="flex-1 truncate">
+                            <div class="flex items-center space-x-3">
+                            <h3 class="truncate text-sm font-medium text-gray-900">{{ correo.from }}</h3>
+                            <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-blue-600 ring-1 ring-inset ring-green-600/20">{{ correo.subject }}</span>
+                            </div>
+                            <p class="mt-1 truncate text-sm text-gray-500">{{ correo.subject }}</p>
+                        </div>
+                        <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src="https://qph.cf2.quoracdn.net/main-thumb-554097988-200-xietklpojlcioqxaqgcyykzfxblvoqrb.jpeg" alt="">
+                        </div>
+                        <div>
+                        <div class="-mt-px flex divide-x divide-gray-200">
+                            <div class="flex w-0 flex-1">
+                            <a href="howpossible17@example.com" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+                                <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+                                </svg>
+                                Email
+                            </a>
+                            </div>
+                            <div class="-ml-px flex w-0 flex-1">
+                            <a href="tel:+1-202-555-0170" class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
+                                </svg>
+                                Call
+                            </a>
+                            </div>
+                        </div>
+                        </div>
+                        <div> 
+                            <ul>
+                                <li v-for="(adjuntos, index) in correo.attach" :key="index" class="text-xs bg-indigo-100 rounded hover:bg-indigo-300 py-2 px-2 ml-1 mt-1 mb-1"
+                                @click="showFile(correo.adjuntos.index)" >
+                                   <span>{{ adjuntos }}</span>
+                                    <!--  <a v-bind:href="'/storage/pdf_temp/' + adjuntos" target="_blank">{{ adjuntos }}</a> -->
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                   
+                    <!-- More people... -->
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <embed 
+                    :src="'/storage/pdf_temp/'+fileSelected" width="1250" height="500" type="application/pdf">
                 </div>
             </div>
         </div>
